33d4533941259e8bc1008bec6186f5037b8f7064
Patrick Rouleau <pfrouleau@gmail.com>
2021-02-06 13:37:19 -0500

    squash! Add scripts to automate the creation of a virtualbox VM
    Fix VM creation... The 2nd SATA disk was added while I was playing with the
    VM and I didn't retry the script before committing.

--- create_virtualbox_vm.sh
+++ create_virtualbox_vm.sh
@@ -20,11 +20,12 @@ VBoxManage modifyvm $MACHINENAME --memory 2048 --vram 128
 VBoxManage modifyvm $MACHINENAME --nic1 nat
 
 # Create Disk and connect Iso
-VBoxManage createhd --filename "$(pwd)/$MACHINENAME/$MACHINENAME_sda.vdi" --size 8000 --format VDI
-VBoxManage createhd --filename "$(pwd)/$MACHINENAME/$MACHINENAME_sdb.vdi" --size 8000 --format VDI
 VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$(pwd)/$MACHINENAME/$MACHINENAME_sda.vdi"
-VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 1 --type hdd --medium "$(pwd)/$MACHINENAME/$MACHINENAME_sdb.vdi"
+VBoxManage createhd --filename "$(pwd)/$MACHINENAME/sda.vdi" --size 8000 --format VDI
+VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$(pwd)/$MACHINENAME/sda.vdi"
+VBoxManage createhd --filename "$(pwd)/$MACHINENAME/sdb.vdi" --size 8000 --format VDI
+VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 1 --device 0 --type hdd --medium "$(pwd)/$MACHINENAME/sdb.vdi"
+
 VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
 VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$(pwd)/$ISO_NAME"
 VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
