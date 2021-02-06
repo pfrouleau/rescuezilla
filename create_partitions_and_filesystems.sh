5e258eabfba7f0524ac6d84ae0262bc43e064f61
Patrick Rouleau <pfrouleau@gmail.com>
2021-02-06 15:25:41 -0500

    squash! Add scripts to automate the creation of a virtualbox VM
    Use sfdisk instead of hacking our way with fdisk.
    Partition sdb with GPT. The 6th partition is of an unknown type.

--- create_partitions_and_filesystems.sh
+++ create_partitions_and_filesystems.sh
@@ -4,7 +4,7 @@
 if [[ -z "$(dmidecode | grep -i "Product Name: VirtualBox")" ]]
 then
     echo "WARNING: no VM detected!"
-    echo "Refusing to run to avoid to corrupt the partitions"
+    echo "Refusing to run to avoid to corrupt the partitions."
     exit 1
 fi >&2
 
@@ -15,48 +15,35 @@ then
     exit 42
 fi
 
-# Source: https://superuser.com/questions/332252/how-to-create-and-format-a-partition-using-a-bash-script
-# to create the partitions programatically (rather than manually)
-# we're going to simulate the manual input to fdisk
-# The sed script strips off all the comments so that we can
-# document what we're doing in-line with the actual commands
-# Note that a blank line (commented as "defualt" will send a empty
-# line terminated with a newline to take the fdisk default.
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk
-  +100M # 100 MB boot partition
-  n # new partition
-  p # primary partition
-  2 # partition number 2
-    # default, start immediately after preceding partition
-  +200M #
-  n # new partition
-  p # primary partition
-  3 # partition number 3
-    # default, start immediately after preceding partition
-  +200M #
-  t # change partition type
-    # default, partition 3
-  b # FAT32
-  n # new partition
-  e # extended partition, implicitly number 4
-    # default, start immediately after the preceding partition
-    # default, extend partition to end of disk
-  n # new partition
-    # default, start at the begining of the extended partition
-  +200M #
-  t # change partition type
-    # default, partition 5
-  7 # NTFS/ExFAT
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
-  w # write the partition table
-  q # and we're done
+cat << EOF | sfdisk /dev/sda
+label: dos
+label-id: 0x16787f0b
+device: /dev/sda
+unit: sectors
+sector-size: 512
+
+/dev/sda1 : start=        2048, size=      204800, type=83, bootable
+/dev/sda2 : start=      206848, size=      409600, type=83
+/dev/sda3 : start=      616448, size=      409600, type=b
+/dev/sda4 : start=     1026048, size=    15357952, type=5
+/dev/sda5 : start=     1028096, size=      409600, type=7
+EOF
+
+cat << EOF | sfdisk /dev/sdb
+label: gpt
+label-id: 07121DFB-DD43-2E4B-B492-5B358C373205
+device: /dev/sdb
+unit: sectors
+first-lba: 2048
+last-lba: 16383966
+sector-size: 512
+
+/dev/sdb1 : start=        2048, size=      204800, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=72BE5BC4-0EEB-8642-9F63-A3FA7203F8C5
+/dev/sdb2 : start=      206848, size=      409600, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=696A73B3-E7BB-1348-8091-6F44D605307D
+/dev/sdb3 : start=      616448, size=      409600, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=35300CDA-2507-C74F-9A2C-08D3BDD9D4D0
+/dev/sdb4 : start=     1026048, size=      409600, type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F, uuid=473A9E65-9066-1242-9CC9-3F80BBAE58B2
+/dev/sdb5 : start=     1435648, size=      409600, type=0FC63DAF-8483-4772-8E79-3D69D8477DE4, uuid=9C8FE3B3-185E-734C-9EFD-C0B37DC897C9
+/dev/sdb6 : start=     1845248, size=      102400, type=00DEAD00-D0D0-FEED-F00D-00DEADBEEF00, uuid=BC8FE393-0000-C0C0-0000-B37DC897C900
 EOF
 
 partprobe
@@ -68,13 +55,21 @@ mkfs.ext4 -L RootExt4   /dev/sda2
 mkfs.vfat -n DATA_FAT32 /dev/sda3
 mkfs.ntfs -L DATA_NTFS  /dev/sda5
 
+mkfs.exfat   -n exFAT   /dev/sdb1
+mkfs.btrfs   -L BTRFS   /dev/sdb2
+mkfs.reiser4 -L REISER4 /dev/sdb3
+mkfs.xfs     -L XFS     /dev/sdb5
+
 # Copy some data in the partitions
 
 mkdir -p dst
-for p in 1 2 3 5
+for d in a b
 do
-    mount /dev/sda$p dst &&
-    rsync -aq /rofs/usr/bin dst/
-    umount dst
+    for p in 1 2 3 5
+    do
+        mount /dev/sd$d$p dst &&
+        rsync -aq /rofs/usr/bin dst/
+        umount dst
+    done
 done
 rmdir dst
